995f07619d3ffe7601a2d9e14501d034a735c4c8
Alessandro Benetton <aleben98@gmail.com>
2023-04-12 11:55:45 +0200

    [main] Fix #2, leave does not require X,Y

--- src/ambient.erl
+++ src/ambient.erl
@@ -13,40 +13,40 @@
 -export([main/2, ambient/1]).
 
 ambient(A) ->
-  receive
-    {isFree, PID, X, Y, Ref} ->
-      io:format("~p: Richiedo ~p ~p~n", [PID,X,Y]),
-      PID ! {status, Ref, lists:member({X,Y,free},A)},
-      ambient(A);
-    {park, PID, X, Y, Ref} ->
-      io:format("~p: Parcheggio ~p ~p~n", [PID,X,Y]),
-      % TODO: Implement deadlock solver
-      case lists:member({X,Y,free},A) of
-        true ->
-          PID ! {parkok, Ref},
-          ambient([{X,Y,Ref} | A -- [{X, Y, free}]]);
-        false ->
-          % TODO: Gotta kill em all
-          PID ! {parkfailed, Ref},
-          ambient(A)
-      end;
-    {leave, PID, X, Y, Ref} ->
-      io:format("~p: Libero ~p ~p~n", [PID,X,Y]),
-      case lists:member({X,Y,Ref},A) of
-        true ->
-          PID ! {leaveok, Ref},
-          ambient([{X,Y,free} | A -- [{X, Y, Ref}]]);
-        false ->
-          % How did you get here?
-          % TODO: Gotta kill em all
-          PID ! {leavefailed, Ref}
-      end,
-      ambient(A)
-  end.
+    receive
+        {isFree, PID, X, Y, Ref} ->
+            io:format("~p: Richiedo ~p ~p~n", [PID, X, Y]),
+            PID ! {status, Ref, lists:member({X, Y, free}, A)},
+            ambient(A);
+        {park, PID, X, Y, Ref} ->
+            io:format("~p: Parcheggio ~p ~p~n", [PID, X, Y]),
+            % TODO: Implement deadlock solver
+            case lists:member({X, Y, free}, A) of
+                true ->
+                    PID ! {parkok, Ref},
+                    ambient([{X, Y, Ref} | A -- [{X, Y, free}]]);
+                false ->
+                    % TODO: Gotta kill em all
+                    PID ! {parkfailed, Ref},
+                    ambient(A)
+            end;
+        {leave, PID, Ref} ->
+            Elem = lists:keyfind(Ref, 3, A), % Finds in the list A the touple that has as third element Ref.
+            case Elem of
+                {X, Y, Ref} ->
+                    io:format("~p: Libero ~p ~p | ~p~n", [PID, X, Y, Ref]),
+                    PID ! {leaveok, Ref},
+                    ambient([{X, Y, free} | A -- [Elem]]);
+                false ->
+                    % How did you get here?
+                    % TODO: Gotta kill em all
+                    PID ! {leavefailed, Ref}
+            end,
+            ambient(A)
+    end.
 
 main(W, H) ->
-  A = [ {R,C,free} || R<-lists:seq(1,W), C<-lists:seq(1, H) ],
-  PID = spawn(?MODULE, ambient, [A]),
-  io:format("Creato ambiente con ~p ~n", [PID]),
-  register(ambient, PID)
-.
+    A = [{R, C, free} || R <- lists:seq(1, W), C <- lists:seq(1, H)],
+    PID = spawn(?MODULE, ambient, [A]),
+    io:format("Creato ambiente con ~p ~n", [PID]),
+    register(ambient, PID).
