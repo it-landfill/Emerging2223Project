912375678b9955191c7e8c76a6f211da6e95b6bb
Nemesis <lorenzo.balugani@gmail.com>
2023-05-10 12:50:01 +0200

    :sparkles: Ambient now keeps track of cars that self-combust while parked
    Using monitors and keeping track of car pids, the system keeps track of dead cars and frees up space.

--- src/ambient.erl
+++ src/ambient.erl
@@ -16,15 +16,16 @@ ambient(A) ->
     receive
         {isFree, PID, X, Y, Ref} ->
             io:format("AMBIENT ~p: Richiedo ~p (~p,~p) | ~p~n", [self(), PID, X, Y, Ref]),
-            PID ! {status, Ref, lists:member({X, Y, free}, A)},
+            PID ! {status, Ref, lists:member({X, Y, free, none, none}, A)},
             ambient(A);
         {park, PID, X, Y, Ref} ->
             io:format("AMBIENT ~p: Parcheggio ~p (~p,~p) | ~p~n", [self(), PID, X, Y, Ref]),
             %TODO: chiedere al prof. Coen come gestire il caso in cui una macchina muoia e occupi il parcheggio. E' necessario memorizzare anche il PID della macchina?
-            case lists:member({X, Y, free}, A) of
+            case lists:member({X, Y, free, none, none}, A) of
                 true ->
                     PID ! {parkOk, Ref},
-                    ambient([{X, Y, Ref} | A -- [{X, Y, free}]]);
+                    MRef = monitor(process, PID),
+                    ambient([{X, Y, Ref, MRef, PID} | A -- [{X, Y, free, none, none}]]);
                 false ->
                     io:format("AMBIENT ~p: Parcheggio ~p (~p,~p) | ~p FALLITO ~n", [self(), PID, X, Y, Ref]),
                     PID ! {parkFailed, Ref},
@@ -34,20 +35,32 @@ ambient(A) ->
             % Finds in the list A the touple that has as third element Ref.
             Elem = lists:keyfind(Ref, 3, A),
             case Elem of
-                {X, Y, Ref} ->
+                {X, Y, Ref, MRef, CARPID} ->
                     io:format("AMBIENT ~p: Libero ~p (~p,~p) | ~p~n", [self(), PID, X, Y, Ref]),
                     PID ! {leaveOk, Ref},
-                    ambient([{X, Y, free} | A -- [Elem]]);
+                    demonitor(MRef),
+                    ambient([{X, Y, free, none, none} | A -- [Elem]]);
                 false ->
                     % How did you get here?
                     % TODO: Gotta kill em all
                     PID ! {leaveFailed, Ref}
             end,
-            ambient(A)
+            ambient(A);
+        {'DOWN', MRef, _, PID, Reason} ->
+            Elem = lists:keyfind(PID, 5, A),
+            io:format("AMBIENT ~p: Auto ~p è morta, libero il posteggio... ~n",[self(), PID]),
+            % Is this proper?
+            case Elem of
+                {X, Y, Ref, MRef, CARPID} ->
+                    ambient([{X, Y, free, none, none} | A -- [Elem]]);
+                false ->
+                    io:format("AMBIENT ~p: Auto ~p è morta, ma il posteggio era gia' stato liberato...? ~p ~p~n",[self(), PID, Elem, A]),
+                    ambient(A)
+            end
     end.
 
 main(W, H, PIDMain) ->
-    A = [{R, C, free} || R <- lists:seq(0, W-1), C <- lists:seq(0, H-1)],
+    A = [{R, C, free, none, none} || R <- lists:seq(0, W-1), C <- lists:seq(0, H-1)],
     PID = spawn(?MODULE, ambient, [A]),
     io:format("SYS Creato ambiente con ~p ~n", [PID]),
     register(ambient, PID),
